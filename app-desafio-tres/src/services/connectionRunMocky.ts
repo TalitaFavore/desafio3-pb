aec668dfc9463fe50287211d88f7b1a33b2db135
TalitaFavore <talitafavore@gmail.com>
2024-06-10 17:26:16 -0300

    feat: configure mock data for API connections

---
+++ app-desafio-tres/src/services/connectionRunMocky.ts
@@ -0,0 +1,17 @@
+/*import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'https://run.mocky.io/v3/',
+  timeout: 10000, 
+});
+
+export const getProducts = async () => {
+  try {
+    const response = await apiClient.get('your-endpoint-id'); 
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw error;
+  }
+};
+*/
---
+++ app-desafio-tres/src/services/connectionViaCEP.ts
@@ -0,0 +1,17 @@
+/*import axios from 'axios';
+
+const viacepApi = axios.create({
+  baseURL: 'https://viacep.com.br/ws/',
+  timeout: 10000, // Timeout de 10 segundos
+});
+
+export const getAddressByCep = async (cep: string) => {
+  try {
+    const response = await viacepApi.get(`${cep}/json`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching address:', error);
+    throw error;
+  }
+};
+*/
