bcee5ea984182f8bc47deae0e880cdf568ba6f12
TalitaFavore <talitafavore@gmail.com>
2024-06-11 16:26:15 -0300

    feat: connecting firebase and firebase auth

--- app-desafio-tres/src/services/connectionFirebase.ts
+++ app-desafio-tres/src/services/connectionFirebase.ts
@@ -1,17 +1,16 @@
-/*import firebase from 'firebase/app';
-import 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDAD-9IcVMVm3SYdm-tAcNFkHMXyLX0sPE",
+  authDomain: "pb-desafio3.firebaseapp.com",
+  projectId: "pb-desafio3",
+  storageBucket: "pb-desafio3.appspot.com",
+  messagingSenderId: "275985839066",
+  appId: "1:275985839066:web:4c45df1cb306e5425486e3"
 };
 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-const auth = firebase.auth();
-
-export {auth};
-*/
+export { app, auth };
