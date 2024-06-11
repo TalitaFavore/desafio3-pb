f9c911801f1966488f4a7fa294bd043588737761
TalitaFavore <talitafavore@gmail.com>
2024-06-11 17:14:41 -0300

    feat: configure project colors

--- app-desafio-tres/tailwind.config.js
+++ app-desafio-tres/tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+      sand: "#FCF8F3",
+      sand3: "#FFF3E3",
+      sand4: "#FAF3EA",
+      primary: "#B88E2F",
+      secondary: "#F9F1E7",
+      black: "#000000",
+      black2: "#242424",
+      gray: "#9F9F9F",
+    
+    },
   },
   plugins: [],
+}
 }
