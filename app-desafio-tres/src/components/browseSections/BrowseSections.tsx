90b62f7561b71204ca89da1183e20ee163cf9db8
TalitaFavore <talitafavore@gmail.com>
2024-06-13 11:25:06 -0300

    feat: structure browse sections component

---
+++ app-desafio-tres/src/components/browseSections/BrowseSections.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const BrowseSections = () => {
+  return (
+    <div>
+      <h1>Browse The Range</h1>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+
+      <div>
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/dining.png" alt="Foto" />
+        <p>Dining</p>
+      </div>
+
+      <div>
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/living.png" alt="Foto" />
+        <p>Living</p>
+      </div>
+
+      <div>
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/bedroom.png" alt="Foto" />
+        <p>Bedroom</p>
+      </div>
+    </div>
+  )
+}
+
+export default BrowseSections;
--- app-desafio-tres/src/pages/HomeScreen.tsx
+++ app-desafio-tres/src/pages/HomeScreen.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/header/Navbar';
 import Hero from '../components/hero/Hero';
+import BrowseSections from '../components/browseSections/BrowseSections';
 
 const LoginScreen: React.FC = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <BrowseSections/>
       <p>Hello world!</p>
     </div>
   );
