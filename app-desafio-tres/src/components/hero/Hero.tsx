b0e66fa11b8ee5aa4e17bd56b5fc0808e568453e
TalitaFavore <talitafavore@gmail.com>
2024-06-12 21:56:15 -0300

    feat: structure hero component

---
+++ app-desafio-tres/src/components/hero/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <main className="bg-custom">
+      <div className="w-14">
+        <p>New Arrival</p>
+        <h1>Discover Our New Collection</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+      </div>
+      <a href="/shop">BUY NOW</a>
+    </main>
+  );
+};
+
+export default Hero;
--- app-desafio-tres/src/pages/HomeScreen.tsx
+++ app-desafio-tres/src/pages/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/header/Navbar';
+import Hero from '../components/hero/Hero';
 
 const LoginScreen: React.FC = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
       <p>Hello world!</p>
     </div>
   );
