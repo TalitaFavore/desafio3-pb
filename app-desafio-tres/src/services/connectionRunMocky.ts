6a0fd04e265727d117c1ea23bb21222a53c178f2
TalitaFavore <talitafavore@gmail.com>
2024-06-13 16:07:41 -0300

    feat: add axios connection and run mocky connection

--- app-desafio-tres/src/services/connectionRunMocky.ts
+++ app-desafio-tres/src/services/connectionRunMocky.ts
@@ -1,19 +1,16 @@
-/*import axios from 'axios';
+import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: 'https://run.mocky.io/v3/',
-  timeout: 10000, 
+  timeout: 10000,
 });
 
 export const getProducts = async () => {
   try {
-    const response = await apiClient.get('your-endpoint-id'); 
+    const response = await apiClient.get('19aa9d64-f673-4027-9692-c71b86db2927');
     return response.data;
   } catch (error) {
     console.error('Error fetching products:', error);
     throw error;
   }
 };
-*/
-
-export {};
