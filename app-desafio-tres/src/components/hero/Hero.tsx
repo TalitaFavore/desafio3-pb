b7150049ee01930e2c90691b0f41efec8b71baa6
TalitaFavore <talitafavore@gmail.com>
2024-06-13 11:25:06 -0300

    feat: style hero component

--- app-desafio-tres/src/components/hero/Hero.tsx
+++ app-desafio-tres/src/components/hero/Hero.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <main className="bg-custom">
-      <div className="w-14">
-        <p>New Arrival</p>
-        <h1>Discover Our New Collection</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+    <main className="flex bg-custom font-poppins justify-end h-screen mt-6">
+      <div className="bg-sand3 flex flex-col justify-between h-3/5 w-2/5 p-10 pt-14 pr-16 m-auto mr-14">
+        <p className="font-semibold">New Arrival</p>
+        <h1 className="font-bold text-5xl w-4/5 text-primary">Discover Our New Collection</h1>
+        <p className="font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        <a className="bg-primary flex p-10 pt-4 pb-4 w-2/5 justify-center text-white font-bold" href="/shop">BUY NOW</a>
       </div>
-      <a href="/shop">BUY NOW</a>
+      
     </main>
   );
 };
