4945cb757877a6090d98894bf9bfff0882ed29ee
TalitaFavore <talitafavore@gmail.com>
2024-06-14 16:49:35 -0300

    feat: add ProductsList to ShopScreen with pagination

--- app-desafio-tres/src/components/productsCard/ProductsList.tsx
+++ app-desafio-tres/src/components/productsCard/ProductsList.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import ProductsCard from "./ProductsCard";
-import { getProducts } from "../../services/connectionRunMocky";
+import React, { useEffect, useState } from 'react';
+import ProductsCard from './ProductsCard';
+import { getProducts } from '../../services/connectionRunMocky';
 
 interface Product {
   id: number;
@@ -14,32 +14,38 @@ interface Product {
 }
 
 interface ProductListProps {
-  displayCount: number;
+  productsPerPage: number;
+  currentPage: number;
 }
 
-const ProductList = ({ displayCount }: ProductListProps) => {
+function ProductsList(props: ProductListProps) {
+  const { productsPerPage, currentPage } = props;
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const productsData = await getProducts();
-        setProducts(productsData.slice(0, displayCount));
+        setProducts(productsData);
       } catch (error) {
         console.error("Error fetching products:", error);
       }
     };
 
     fetchProducts();
-  }, [displayCount]);
+  }, []);
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {products.map((product) => (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+      {currentProducts.map((product) => (
         <ProductsCard key={product.id} product={product} />
       ))}
     </div>
   );
-};
+}
 
-export default ProductList;
+export default ProductsList;
--- app-desafio-tres/src/components/productsCard/ShopProducts.tsx
+++ app-desafio-tres/src/components/productsCard/ShopProducts.tsx
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductList from './ProductsList';
+import { getProducts } from '../../services/connectionRunMocky';
 
 const ShopProducts = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const productsPerPage = 16;
+
+  useEffect(() => {
+    const fetchTotalPages = async () => {
+      try {
+        const productsData = await getProducts();
+        const totalPages = Math.ceil(productsData.length / productsPerPage);
+        setTotalPages(totalPages);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchTotalPages();
+  }, []);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const pageButtons = Array.from({ length: totalPages }, (_, index) => index + 1);
+
   return (
-    <main className="flex flex-col items-center">
-      <ProductList displayCount={16} />
+    <main className="flex flex-col items-center font-poppins">
+      <ProductList productsPerPage={productsPerPage} currentPage={currentPage} />
+      <div className="flex gap-6 mt-12 mb-12 ">
+        {currentPage > 1 && (
+          <button
+            onClick={handlePreviousPage}
+            className="px-4 py-2 bg-secondary rounded hover:bg-primary hover:text-white"
+          >
+            Previous
+          </button>
+        )}
+        {pageButtons.map((page) => (
+          <button
+            key={page}
+            onClick={() => handlePageChange(page)}
+            className={`px-4 py-2 rounded ${currentPage === page ? 'bg-primary text-white' : 'bg-secondary '} hover:bg-primary hover:text-white`}
+          >
+            {page}
+          </button>
+        ))}
+        {!((currentPage === totalPages) || (currentPage > totalPages)) && (
+          <button
+            onClick={handleNextPage}
+            className="px-4 py-2 bg-secondary  rounded hover:bg-primary hover:text-white disabled:opacity-50"
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        )}
+      </div>
     </main>
   );
 };
