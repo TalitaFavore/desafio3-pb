5c1fa1a90b54c6653efd3afb5d9cdf2ed91393be
TalitaFavore <talitafavore@gmail.com>
2024-06-14 15:12:20 -0300

    style: style filterbar component

--- app-desafio-tres/src/components/header/FilterBar.tsx
+++ app-desafio-tres/src/components/header/FilterBar.tsx
@@ -2,25 +2,33 @@ import React from "react";
 
 const FilterBar = () => {
   return (
-    <div>
-      <img
-        src="https://pb-desafio3.s3.us-east-2.amazonaws.com/filter.svg"
-        alt=""
-      />
-      <p>Filter</p>
-      <img
-        src="https://pb-desafio3.s3.us-east-2.amazonaws.com/grid.svg"
-        alt=""
-      />
-      <img
-        src="https://pb-desafio3.s3.us-east-2.amazonaws.com/view-list.svg"
-        alt=""
-      />
-      <p>Showing 1–16 of 32 results</p>
-      <p>Show</p>
-      <input type="number" placeholder="16" />
-      <p>Sort By</p>
-      <input type="text" placeholder="Default" />
+    <div className="flex justify-between pl-20 pr-10 pb-7 pt-7 bg-secondary">
+      <div className="flex w-1/3 justify-between items-center">
+        <img
+          src="https://pb-desafio3.s3.us-east-2.amazonaws.com/filter.svg"
+          alt=""
+          className="w-7"
+        />
+        <p>Filter</p>
+        <img
+          src="https://pb-desafio3.s3.us-east-2.amazonaws.com/grid.svg"
+          alt=""
+          className="w-7"
+        />
+        <img
+          src="https://pb-desafio3.s3.us-east-2.amazonaws.com/view-list.svg"
+          alt=""
+          className="w-7"
+        />
+        <p>Showing 1–16 of 32 results</p>
+      </div>
+
+      <div className="flex w-1/3 items-center">
+        <p>Show</p>
+        <input type="text" placeholder="16" className="w-12 h-12 ml-4 mr-4 p-3"/>
+        <p>Sort By</p>
+        <input type="text" placeholder="Default" className="w-32 h-12 ml-4 mr-4 pl-4" />
+      </div>
     </div>
   );
 };
