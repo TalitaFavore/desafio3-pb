6bccbd84739a6d86d17454a45145584562914fb4
TalitaFavore <talitafavore@gmail.com>
2024-06-14 10:58:40 -0300

    feat: structure share component

---
+++ app-desafio-tres/src/components/share/Share.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Share = () => {
+  return (
+    <main>
+      <h2>Share your setup with</h2>
+      <h1>#FuniroFurniture</h1>
+      <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/share.png" alt="" />
+    </main>
+  )
+}
+
+export default Share;
--- app-desafio-tres/src/pages/HomeScreen.tsx
+++ app-desafio-tres/src/pages/HomeScreen.tsx
@@ -4,6 +4,7 @@ import Hero from '../components/hero/Hero';
 import BrowseSections from '../components/browseSections/BrowseSections';
 import Inspiration from '../components/inspiration/Inspiration';
 import HomeProducts from '../components/productsCard/HomeProducts';
+import Share from '../components/share/Share';
 
 function HomeScreen() {
   return (
@@ -13,6 +14,7 @@ function HomeScreen() {
       <BrowseSections />
       <HomeProducts />
       <Inspiration />
+      <Share/>
       <p>Hello world!</p>
     </div>
   );
