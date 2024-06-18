4a76dfe26c43201ee35b086dd318b1257c780bb0
TalitaFavore <talitafavore@gmail.com>
2024-06-18 15:05:13 -0300

    feat: integrate Redux and adjust component definition

--- app-desafio-tres/src/App.tsx
+++ app-desafio-tres/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux'; 
+import store from './redux/store';
 import AppRoutes from './utils/Route';
 import '@splidejs/splide/dist/css/splide.min.css';
 
-const App: React.FC = () => {
+
+const App = () => {
   return (
-    <Router>
-      <AppRoutes />
-    </Router>
+    <Provider store={store}> 
+      <Router>
+        <AppRoutes />
+      </Router>
+    </Provider>
   );
 };
 
