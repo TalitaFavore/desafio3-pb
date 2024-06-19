f0bd5fcdcf9b245f7a72c00562e5a41bb987e412
TalitaFavore <talitafavore@gmail.com>
2024-06-19 16:02:59 -0300

    feat: structure productDetails component

---
+++ app-desafio-tres/src/components/details/ProductDetails.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <h1>Asgaard sofa</h1>
+      <h2>Rs. 250,000.00</h2>
+      <div>Stars | 5 Customer Review</div>
+      <p>
+        Setting the bar as one of the loudest speakers in its class, the Kilburn
+        is a compact, stout-hearted hero with a well-balanced audio which boasts
+        a clear midrange and extended highs for a sound.
+      </p>
+
+      <div>
+      <p>Size</p>
+      <div>
+        <button>L</button>
+        <button>XL</button>
+        <button>XS</button>
+      </div>
+      </div>
+      
+      <div>
+      <p>Color</p>
+      <div>
+        <button/>
+        <button/>
+        <button/>
+      </div>
+
+      <div>
+        <button>1</button>
+        <button>Add To Cart</button>
+      </div>
+
+      <hr />
+
+      <div>
+        <p>SKU: SS001</p>
+        <p>Category: Sofas</p>
+        <p>Tags: Sofa, Chair, Home, Shop</p>
+        <p>Share: SS001</p>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default ProductDetails;
--- app-desafio-tres/src/pages/SingleProductScreen.tsx
+++ app-desafio-tres/src/pages/SingleProductScreen.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Description from '../components/description/Description'
 import Navbar from '../components/header/Navbar'
 import Footer from '../components/footer/Footer'
+import ProductDetails from '../components/details/ProductDetails'
 
 const SingleProductScreen = () => {
   return (
     <div>
       <Navbar/>
+      <ProductDetails/>
       <Description/>
       <Footer/>
+      
     </div>
   )
 }
