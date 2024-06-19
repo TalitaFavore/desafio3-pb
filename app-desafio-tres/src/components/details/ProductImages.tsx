121cf3bd1b37acbf8a965212da5b047c708174ea
TalitaFavore <talitafavore@gmail.com>
2024-06-19 19:35:37 -0300

    feat: structure product images component

---
+++ app-desafio-tres/src/components/details/ProductImages.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProductImages = () => {
+  return (
+    <div>
+      <div>
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
+      </div>
+      <div>
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default ProductImages
--- app-desafio-tres/src/pages/SingleProductScreen.tsx
+++ app-desafio-tres/src/pages/SingleProductScreen.tsx
@@ -3,11 +3,13 @@ import Description from '../components/description/Description'
 import Navbar from '../components/header/Navbar'
 import Footer from '../components/footer/Footer'
 import ProductDetails from '../components/details/ProductDetails'
+import ProductImages from '../components/details/ProductImages'
 
 const SingleProductScreen = () => {
   return (
     <div>
       <Navbar/>
+      <ProductImages/>
       <ProductDetails/>
       <Description/>
       <Footer/>
