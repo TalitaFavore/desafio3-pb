0bd23a94727b1510abbf1968ed25628fe3c008a6
TalitaFavore <talitafavore@gmail.com>
2024-06-19 19:42:02 -0300

    style: style product images component

--- app-desafio-tres/src/components/details/ProductImages.tsx
+++ app-desafio-tres/src/components/details/ProductImages.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const ProductImages = () => {
   return (
-    <div>
-      <div>
-        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
-        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
-        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
-        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
+    <div className='flex flex-row'>
+      <div className='flex flex-col w-16 mr-8'>
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" className='mb-5' alt="" />
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" className='mb-5' alt="" />
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" className='mb-5' alt="" />
+        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" className='mb-5' alt="" />
       </div>
-      <div>
+      <div className='flex w-96 h-96'>
         <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/lolito.png" alt="" />
       </div>
     </div>
