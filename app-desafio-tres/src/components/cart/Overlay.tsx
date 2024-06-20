6153e00afeaebf5f6caa7f016863450eba85a019
TalitaFavore <talitafavore@gmail.com>
2024-06-20 16:25:58 -0300

    style: style overlay component

--- app-desafio-tres/src/components/cart/Overlay.tsx
+++ app-desafio-tres/src/components/cart/Overlay.tsx
@@ -1,22 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const Overlay = () => {
   return (
-    <div>
-        <div>
-        <h1>Shopping Cart</h1>
-        <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/bag.svg" alt="" />
-        </div>
-      
-      <hr />
-      <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/leviosa.png" alt="" />
+    <div className=" flex flex-col justify-between font-poppins p-5 h-750">
       <div>
-        <h2>Asgaard sofa</h2>
-        <p> 1 X Rs. 250,000.00</p>
+        <div className="flex ">
+          <h1 className="text-2xl font-semibold mr-40">Shopping Cart</h1>
+          <img
+            src="https://pb-desafio3.s3.us-east-2.amazonaws.com/bag.svg"
+            alt=""
+          />
+        </div>
+        <hr className="w-2/3 mt-5 mb-10" />
+        <div className="flex items-center">
+          <img
+            src="https://pb-desafio3.s3.us-east-2.amazonaws.com/leviosa.png"
+            alt=""
+            className="w-24 h-24 rounded-lg"
+          />
+          <div className="flex flex-col ml-6 mr-16">
+            <h2>Asgaard sofa</h2>
+            <div className="flex gap-4 items-center">
+              <p> 1 </p>
+              <p className="font-light text-sm">X </p>
+              <p className="text-primary">Rs. 250,000.00</p>
+            </div>
+          </div>
+          <img
+            src="https://pb-desafio3.s3.us-east-2.amazonaws.com/close.svg"
+            alt=""
+          />
+        </div>
       </div>
-      <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/close.svg" alt="" />
-    </div>
-  )
-}
 
-export default Overlay
+      <div>
+        <div className="flex gap-24">
+          <p>Subtotal</p>
+          <p className="text-primary font-semibold">Rs. 520,000.00</p>
+        </div>
+
+        <hr className="w-full border-gray8 mt-8 mb-8" />
+        <div className="flex justify-between">
+          <button className="border rounded-2xl pl-6 pr-6 pt-1 pb-1 text-sm">
+            Cart
+          </button>
+          <button className="border rounded-2xl pl-6 pr-6 pt-1 pb-1 text-sm">
+            Checkout
+          </button>
+          <button className="border rounded-2xl pl-6 pr-6 pt-1 pb-1 text-sm">
+            Comparison
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Overlay;
