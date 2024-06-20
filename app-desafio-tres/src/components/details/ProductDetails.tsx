e39b0a84e9156117873708c2d03c984280597693
TalitaFavore <talitafavore@gmail.com>
2024-06-19 22:41:38 -0300

    fix: repair data display details

--- app-desafio-tres/src/components/details/ProductDetails.tsx
+++ app-desafio-tres/src/components/details/ProductDetails.tsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import NumericInput from "./NumericInput";
+import Stars from "./Stars";
 
 interface Product {
   id: string;
@@ -8,7 +10,6 @@ interface Product {
   description: string;
   price: number;
   stars: number;
-  reviews: number;
   sizes: string[];
   colors?: { name: string; hex: string }[];
   sku: string;
@@ -16,7 +17,7 @@ interface Product {
   tags: string[];
 }
 
-const ProductDetails: React.FC = () => {
+const ProductDetails = () => {
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [selectedSize, setSelectedSize] = useState<string | null>(null);
@@ -29,7 +30,7 @@ const ProductDetails: React.FC = () => {
         const data = await response.json();
         setProduct(data);
       } catch (error) {
-        console.error('Erro ao carregar o produto:', error);
+        console.error("Erro ao carregar o produto:", error);
       }
     };
 
@@ -39,10 +40,6 @@ const ProductDetails: React.FC = () => {
   if (!product) {
     return <div>Loading...</div>;
   }
-
-  // Agrupa as atribuições em uma única linha
-  const colors = product.colors || [];
-  const sizes = product.sizes || [];
 
   const handleSizeSelect = (size: string) => {
     setSelectedSize(size);
@@ -55,20 +52,25 @@ const ProductDetails: React.FC = () => {
   return (
     <div className="font-poppins w-1/3">
       <h1 className="text-3xl">{product.name}</h1>
-      <h2 className="font-medium text-gray mb-4">Rs. {product.price.toLocaleString()}</h2>
+      <h2 className="font-medium text-gray mb-4">
+        Rs. {product.price.toLocaleString()}
+      </h2>
       <div className="text-gray flex items-center">
-         <span className="ml-2">| {product.reviews} Customer Review</span>
+        <Stars stars={product.stars} />
+        <span className="ml-2">| 5 Customer Review</span>
       </div>
       <p className="mt-4 mb-5 w-4/5">{product.description}</p>
 
       <div>
         <p className="mb-3 text-gray">Size</p>
         <div className="flex space-x-2">
-          {sizes.map((size) => (
+          {["L", "XL", "XS"].map((size) => (
             <button
               key={size}
               onClick={() => handleSizeSelect(size)}
-              className={`w-10 h-10 rounded ${selectedSize === size ? 'bg-primary text-white' : 'bg-secondary'} hover:bg-primary hover:text-white`}
+              className={`w-10 h-10 rounded ${
+                selectedSize === size ? "bg-primary text-white" : "bg-secondary"
+              } hover:bg-primary hover:text-white`}
             >
               {size}
             </button>
@@ -76,28 +78,32 @@ const ProductDetails: React.FC = () => {
         </div>
       </div>
 
-      {colors.length > 0 && (
-        <div>
-          <p className="mt-3 mb-3 text-gray">Color</p>
-          <div className="flex space-x-2">
-            {colors.map((color) => (
-              <button
-                key={color.name}
-                onClick={() => handleColorSelect(color.hex)}
-                className={`p-2 rounded-full ${selectedColor === color.hex ? 'border-2 border-gray5' : ''}`}
-                style={{
-                  backgroundColor: color.hex,
-                  width: '30px',
-                  height: '30px',
-                }}
-              />
-            ))}
-          </div>
+      <div>
+        <p className="mt-3 mb-3 text-gray">Color</p>
+        <div className="flex gap-5">
+          {[
+            { name: "Purple", hex: "#816DFA" },
+            { name: "Black", hex: "#000000" },
+            { name: "Brown", hex: "#B88E2F" },
+          ].map((color) => (
+            <button
+              key={color.name}
+              onClick={() => handleColorSelect(color.hex)}
+              className={`p-2 rounded-full ${
+                selectedColor === color.hex ? "border-2 border-gray-500" : ""
+              }`}
+              style={{
+                backgroundColor: color.hex,
+                width: "30px",
+                height: "30px",
+              }}
+            />
+          ))}
         </div>
-      )}
+      </div>
 
       <div className="flex items-center space-x-4 my-4">
-        <button>1</button>
+        <NumericInput initialValue={1} min={1} max={100} />
         <button className="pl-8 pr-8 pt-3 pb-3 rounded-xl text-black border hover:bg-secondary hover:text-black">
           Add To Cart
         </button>
@@ -108,12 +114,21 @@ const ProductDetails: React.FC = () => {
       <div className="text-gray">
         <p className="mb-2">SKU: {product.sku}</p>
         <p className="mb-2">Category: {product.category}</p>
-        <p className="mb-2">Tags: {product.tags.join(', ')}</p>
+        <p className="mb-2">Tags: {product.tags.join(", ")}</p>
         <div className="flex gap-4">
           <p className="mb-2">Share:</p>
-          <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/icons_facebook.svg" alt="" />
-          <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/icons_linkedin.svg" alt="" />
-          <img src="https://pb-desafio3.s3.us-east-2.amazonaws.com/icons_twitter.svg" alt="" />
+          <img
+            src="https://pb-desafio3.s3.us-east-2.amazonaws.com/icons_facebook.svg"
+            alt=""
+          />
+          <img
+            src="https://pb-desafio3.s3.us-east-2.amazonaws.com/icons_linkedin.svg"
+            alt=""
+          />
+          <img
+            src="https://pb-desafio3.s3.us-east-2.amazonaws.com/icons_twitter.svg"
+            alt=""
+          />
         </div>
       </div>
     </div>
